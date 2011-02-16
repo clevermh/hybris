6b2ec9740e6c86155b85d80838c92688f0231e2d
showdown345@gmail.com <showdown345@gmail.com@f1c88aba-0dde-0a15-9544-746dbaf2be2f>
2011-02-16 09:40:54 +0000

    Fixed up the UPC Database by adding more entities into it.

--- trunk/src/com/kumquat/hybris/UPCDatabaseHelper.java
+++ trunk/src/com/kumquat/hybris/UPCDatabaseHelper.java
@@ -6,26 +6,32 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class UPCDatabaseHelper extends SQLiteOpenHelper {
-	private SQLiteDatabase database;
-	//private final Context context;
+	private SQLiteDatabase upc_database;
 	public static final int VERSION = 3;
 	
-	private static final String createDB = "CREATE TABLE upctable (" +
-											"upc			varchar(12) primary key" +
-											", item			varchar(5000)" +
-											", amount		int" +
-											", amount_type	varchar(255)" +
+	private static final String upc_table = "CREATE TABLE IF NOT EXISTS upctable (" +
+											"upc_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+											"upc_code varchar(12) NOT NULL default ''," +
+											"upc_e varchar(8) NOT NULL default ''," +
+											"ean_code varchar(13) NOT NULL default ''," +
+											"description varchar(2000) NOT NULL default ''," +
+											"product_type varchar(30) NOT NULL default ''," +
+											"amount varchar(200) NOT NULL default ''," +
+											"company varchar(200) NOT NULL default ''," +
+											"country_issued varchar(200) NOT NULL default ''" +
 											");";
+	private static final String upc_idx = "CREATE INDEX IF NOT EXISTS upc_idx_01 ON upctable(upc_code);";
+	
 	
 	public UPCDatabaseHelper(Context context) {
 		super(context, "UPCDatabase", null, VERSION);
-		//this.context = context;
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		database = db;
-		database.execSQL(createDB);
+		upc_database = db;
+		upc_database.execSQL(upc_table);
+		upc_database.execSQL(upc_idx);
 	}
 
 	@Override
@@ -35,4 +41,5 @@ public class UPCDatabaseHelper extends SQLiteOpenHelper {
 		db.execSQL("DROP TABLE IF EXISTS upctable");
         onCreate(db);
 	}
+	
 }
