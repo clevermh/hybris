ce82fb66072fb1709f9cf74ec156925d29aeafb2
showdown345 <showdown345@gmail.com>
2011-02-16 09:41:17 +0000

    Interfaced the class with the new UPC database.

--- src/com/kumquat/hybris/Test.java
+++ src/com/kumquat/hybris/Test.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 
 import android.app.Activity;
@@ -81,10 +82,10 @@ public class Test extends Activity {
 			
 			String code = txt.getText().toString();
 			setTextViewText(R.id.code, "Code: " + code);
-			String[] item = getItemFromBarcode(code);
-			if(item != null) {
-            	setTextViewText(R.id.item, "Item: " + item[0]);
-            	setTextViewText(R.id.other, "Amt: " + item[1] + "\nType: " + item[2]);
+			UPCObject upc_object = getItemFromBarcode(code);
+            if(upc_object != null) {
+            	setTextViewText(R.id.item, "Item: " + upc_object.getDescription());
+            	setTextViewText(R.id.other, "Amt: " + upc_object.getAmount() + "\nType: " + upc_object.getProductType());
             }
 		}
 	};
@@ -94,7 +95,9 @@ public class Test extends Activity {
 		public void onClick(View v) {
 			toaster("Dumping DB to file").show();
 			SQLiteDatabase db = dbhelper.getWritableDatabase();
-			Cursor c = db.query("upctable", null, "1 = 1", null, null, null, null);
+			
+			String sql_statement = "SELECT * FROM upctable";
+	        Cursor c = db.rawQuery(sql_statement, null);
 			
 			if(c != null && c.getCount() > 0) {
 				c.moveToFirst();
@@ -129,21 +132,26 @@ public class Test extends Activity {
 		}
 	};
 	
-	private String[] getItemFromBarcode(String code) {
-		String[] res = getItemByDatabase(code);
+	private UPCObject getItemFromBarcode(String code) {
+		UPCObject upc_object = getItemByDatabase(code);
 		setTextViewText(R.id.error, "In DB :D");
-		if(res != null) { return res; }
+		if(upc_object != null) { return upc_object; }
 		
 		setTextViewText(R.id.error, "Not in DB :(");
-		res = getItemByInternet(code);
-		if(res != null) {
+		upc_object = getItemByInternet(code);
+		if(upc_object != null) {
 			SQLiteDatabase db = dbhelper.getWritableDatabase();
 			
 			ContentValues cv = new ContentValues();
-			cv.put("upc", code);
-			cv.put("item", res[0]);
-			cv.put("amount", res[1]);
-			cv.put("amount_type", res[2]);
+			cv.put("upc_code", upc_object.getUPCCode());
+			cv.put("upc_e", upc_object.getUPCECode());
+			cv.put("ean_code", upc_object.getEANCode());
+			cv.put("description", upc_object.getDescription());
+			cv.put("product_type", upc_object.getProductType());
+			cv.put("amount", upc_object.getAmount());
+			cv.put("company", upc_object.getCompany());
+			cv.put("country_issued", upc_object.getCountry());
+			
 			try {
 				long ret = db.insertOrThrow("upctable", null, cv);
 				
@@ -151,74 +159,130 @@ public class Test extends Activity {
 					toaster("Error adding " + code).show();
 					Log.e("DB", "Error adding new item");
 					setTextViewText(R.id.error, "Error adding item");
-				}
+				} 
 			} catch (SQLException e) {
 				setTextViewText(R.id.error, e.toString());
 			}
 			db.close();
 			
-			return res;
+			return upc_object;
 		}
 		
 		return null;
 	}
 	
-	private String[] getItemByDatabase(String code) {
-		String[] res = new String[3];
-		
+	private UPCObject getItemByDatabase(String code) {
 		SQLiteDatabase db = dbhelper.getReadableDatabase();
-		Cursor c = db.query("upctable", new String[]{"item", "amount", "amount_type"}, "upc = \"" + code + "\"", null, null, null, null);
+		String sql_statement = "SELECT upc_code, upc_e, ean_code, description, amount, country_issued FROM upctable " +
+								"WHERE upc_code = ?";
+		Cursor c = db.rawQuery(sql_statement, new String[]{code});
 		
 		if(c == null || c.getCount() == 0) { return null; }
 		
+		HashMap <String, String> upc_info = new HashMap<String, String>();
 		c.moveToFirst();
-		res[0] = c.getString(0);
-		res[1] = c.getString(1);
-		res[2] = c.getString(2);
+		upc_info.put("upc_code", c.getString(0));
+		upc_info.put("upc_e", c.getString(1));
+		upc_info.put("ean_code", c.getString(2));
+		upc_info.put("description", c.getString(3));
+		upc_info.put("amount", c.getString(4));
+		upc_info.put("country_issued", c.getString(5));
+		
+		UPCObject upc_object = new UPCObject(upc_info.get("upc_code"));
+        upc_object.addUPCInformation(upc_info);
 		
 		c.close();
 		db.close();
 		
-		return res;
+		return upc_object;
 	}
 	
-	private String[] getItemByInternet(String code) {
+	private String[] splitHtmlPage(String page) {
+		String[] info = page.split("[<>]+");
+
+        int counter = 0;
+        String[] new_info = new String[info.length];
+        for (int i = 0; i < info.length; i++) {
+            String val = info[i].toLowerCase();
+            if (val.compareTo("td") == 0 || val.compareTo("/td") == 0 || val.compareTo("tr") == 0 || val.compareTo("/tr") == 0 
+                || val.indexOf("td width") != -1 || val.compareTo("h2") == 0 || val.compareTo("/h2") == 0 
+                || val.indexOf("script type") != -1 || val.compareTo("/script") == 0 || val.indexOf("table class") != -1) {
+                continue;
+            } else {
+                new_info[counter] = info[i];
+                counter += 1;
+            }
+        }
+        
+        return new_info;
+	}
+	
+	private HashMap <String, String> populateUPCObject(String[] new_info) {
+		HashMap <String, String> upc_info = new HashMap<String, String>();
+        int begin_index = 0;
+        for (int i = 0; i < new_info.length; i++) {
+            String val = new_info[i].toLowerCase();
+
+            if (val.compareTo("item record") == 0) {
+                begin_index = i;
+                break;
+            }
+        }
+
+        for (int i = begin_index; i < new_info.length; i++) {
+            String val = new_info[i].toLowerCase();
+
+            if(val.indexOf("var defaultupc =") != -1) {
+                String[] string_info = new_info[i].split("[\\D]+");
+                upc_info.put("upc_code", string_info[string_info.length-1]);
+            } else if (val.indexOf("upc-e") != -1) {
+                i += 1;
+                String[] string_info = new_info[i].split("[\\D]+");
+                upc_info.put("upc_e", string_info[string_info.length-1]);
+            } else if (val.indexOf("upc-a") != -1) {
+                i += 1;
+                String[] string_info = new_info[i].split("[\\D]+");
+                upc_info.put("upc_code", string_info[string_info.length-1]);
+            } else if (val.indexOf("ean/ucc-13") != -1) {
+                i += 1;
+                String[] string_info = new_info[i].split("[\\D]+");
+                upc_info.put("ean_code", string_info[string_info.length-1]);
+            } else if (val.indexOf("description") != -1) {
+                i += 1;
+                upc_info.put("description", new_info[i]);
+            } else if (val.indexOf("size/weight") != -1) {
+                i += 1;
+                upc_info.put("amount", new_info[i]);
+            } else if (val.indexOf("issuing country") != -1) {
+                i += 1;
+                upc_info.put("country_issued", new_info[i]);
+            } else if (val.compareTo("last modified") == 0) {
+                break;
+            }
+        }
+        
+        return upc_info;
+	}
+	
+	private UPCObject getItemByInternet(String code) {
 		try {
 			URL url = new URL("http://www.upcdatabase.com/item/" + code);
-			HttpURLConnection urlconnect = (HttpURLConnection)url.openConnection();
-			InputStream in = new BufferedInputStream(urlconnect.getInputStream());
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String page = "";
-			String line = br.readLine();
-			while(line != null) {
-				page += line;
-				line = br.readLine();
-			}
-			
-			int st, en;
-			st = page.indexOf("<td>Description");
-			if(st != -1) {
-				st += 15 + 18;
-				en = page.indexOf("</td>", st);
-				
-				String[] res = new String[3];
-				res[0] = page.substring(st, en);
-				
-				st = page.indexOf("<td></td>", en + 2);
-				st += 13;
-				en = page.indexOf("</td>", st);
-				String tmp = page.substring(st, en);
-				
-				String[] tmp2 = tmp.split("\\s", 2);
-				if(tmp2.length == 2) {
-					res[1] = tmp2[0];
-					res[2] = tmp2[1];
-				} else {
-					res[1] = res[2] = "";
-				}
-				
-				return res;
-			}
+            HttpURLConnection urlconnect = (HttpURLConnection)url.openConnection();
+            InputStream in = new BufferedInputStream(urlconnect.getInputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String page = "";
+            String line = br.readLine();
+            while(line != null) {
+                page += line;
+                line = br.readLine();
+            }
+           
+            String[] new_info = splitHtmlPage(page);
+            HashMap <String, String> upc_info = populateUPCObject(new_info);
+            UPCObject upc_object = new UPCObject(upc_info.get("upc_code"));
+            upc_object.addUPCInformation(upc_info);
+ 
+            return upc_object;
 		} catch (MalformedURLException e) {
 			setTextViewText(R.id.error, e.toString());
 			toaster("MalformedURLException").show();
@@ -234,10 +298,10 @@ public class Test extends Activity {
 	    if (requestCode == 0) {
 	        if (resultCode == RESULT_OK) {
 	            setTextViewText(R.id.code, "Code: " + intent.getStringExtra("SCAN_RESULT"));
-	            String[] item = getItemFromBarcode(intent.getStringExtra("SCAN_RESULT"));
-	            if(item != null) {
-	            	setTextViewText(R.id.item, "Item: " + item[0]);
-	            	setTextViewText(R.id.other, "Amt: " + item[1] + "\nType: " + item[2]);
+	            UPCObject upc_object = getItemFromBarcode(intent.getStringExtra("SCAN_RESULT"));
+	            if(upc_object != null) {
+	            	setTextViewText(R.id.item, "Item: " + upc_object.getDescription());
+	            	setTextViewText(R.id.other, "Amt: " + upc_object.getAmount() + "\nType: " + upc_object.getProductType());
 	            }
 	        } else if (resultCode == RESULT_CANCELED) {
 	            // Handle cancel
@@ -276,14 +340,15 @@ public class Test extends Activity {
         toaster(bsavail).show();
         
         if(!scanAvailable) { sc.setEnabled(false); }
-        
         dbhelper = new UPCDatabaseHelper(getApplicationContext());
         
         SQLiteDatabase db = dbhelper.getReadableDatabase();
-		Cursor c = db.query("upctable", new String[]{"upc"}, "1 = 1", null, null, null, null);
-		
-		setTextViewText(R.id.error, "Items in DB: " + c.getCount());
-		
+        String sql_statement = "SELECT COUNT(*) FROM upctable";
+        Cursor c = db.rawQuery(sql_statement, null);
+        c.moveToFirst();
+        
+        //Log.v("error", "size of upctable = " + c.getString(0)); 
+		setTextViewText(R.id.error, "Items in DB: " + c.getString(0));
 		c.close();
 		db.close();
     }
