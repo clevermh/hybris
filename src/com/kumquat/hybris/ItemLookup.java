1fdbc10b74a8e818c1c6b6998b0399c5af90bf5e
kesshoryu927 <kesshoryu927@gmail.com>
2011-02-24 00:19:37 +0000

    OVERRIDE

--- src/com/kumquat/hybris/ItemLookup.java
+++ src/com/kumquat/hybris/ItemLookup.java
@@ -1,13 +1,19 @@
 package com.kumquat.hybris;
 
+import com.kumquat.hybris.databases.*;
+
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class ItemLookup {
-	
-	public static int getItemIDfromCode(String code, SQLiteDatabase db, String sql_statement) {
-		//String sql_statement = "SELECT upc_code, upc_e, ean_code, description, amount FROM upctable " + "WHERE upc_code = ?";
-		Cursor c = db.rawQuery(sql_statement, new String[]{code});
+	public static int getItemIDByDatabase(Context con, String code) {
+		String sql_statement = "SELECT item_id FROM Upc_Table " +
+								"WHERE upc_code = ?";
+		
+		UPCDatabaseHelper udbh = new UPCDatabaseHelper(con);
+		SQLiteDatabase udb = udbh.getReadableDatabase();
+		Cursor c = udb.rawQuery(sql_statement, new String[]{code});
 		
 		if(c == null || c.getCount() == 0) { if(c != null) { c.close(); } return -1; }
 		
@@ -16,7 +22,7 @@ public class ItemLookup {
 		int ItemID = c.getInt(0);
 		
 		c.close();
-		db.close();
+		udb.close();
 		
 		return ItemID;
 	}
