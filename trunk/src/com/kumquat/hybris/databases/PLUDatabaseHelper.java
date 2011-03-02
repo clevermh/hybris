5692afdab3c82fd69141837370656cce3495f911
kesshoryu927@gmail.com <kesshoryu927@gmail.com@f1c88aba-0dde-0a15-9544-746dbaf2be2f>
2011-03-02 23:24:08 +0000

    Made the UPC and PLU databases populate from a file.

--- trunk/src/com/kumquat/hybris/databases/PLUDatabaseHelper.java
+++ trunk/src/com/kumquat/hybris/databases/PLUDatabaseHelper.java
@@ -1,31 +1,95 @@
 package com.kumquat.hybris.databases;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.kumquat.hybris.R;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class PLUDatabaseHelper extends SQLiteOpenHelper {
+	private Context context;
 	private SQLiteDatabase plu_database;
-	public static final int VERSION = 2;
+	public static final int VERSION = 3;
 	
 	private static final String plu_table = "CREATE TABLE IF NOT EXISTS Plu_Table (" +
 											"id INTEGER PRIMARY KEY AUTOINCREMENT," +
 											"item_id INTEGER NOT NULL," +
 											"plu_code varchar(5) default '0'," +
+											"user_added INTEGER default 0," +
 											"FOREIGN KEY (item_id) REFERENCES Items(id)," +
 											"UNIQUE (plu_code)" +
 											");";
 	
 	public PLUDatabaseHelper(Context context) {
 		super(context, "PLUDatabase", null, VERSION);
+		
+		this.context = context;
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		plu_database = db;
 		plu_database.execSQL(plu_table);
-		// populate();
+		
+		populate();
+	}
+	
+	private void populate() {
+		/*new Thread(new Runnable() {
+            public void run() {
+                try {
+                    loadItems();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();*/
+		
+		try {
+            loadItems();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+	}
+	
+	private void loadItems() throws IOException {
+		Log.d("PLUDatabase", "Loading items.");
+		
+		final Resources resources = context.getResources();
+        InputStream inputStream = resources.openRawResource(R.raw.plus);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        
+        try {
+        	String line;
+        	while((line = reader.readLine()) != null) {
+        		if(line.startsWith("#")) { continue; }
+        		
+        		String[] split = line.split("\t");
+        		if(split.length != 2) { continue; }
+        		
+        		ContentValues item = new ContentValues();
+        		item.put("plu_code", split[0].trim());
+        		item.put("item_id", split[1].trim());
+        		
+        		long id = plu_database.insertOrThrow("Plu_Table", null, item);
+        		
+        		if(id == -1) {
+        			Log.e("PLUDatabase", "Unable to add: " + line.trim());
+        		}
+        	}
+        } finally {
+        	reader.close();
+        }
+		
+		Log.d("PLUDatabase", "Done loading items.");
 	}
 
 	@Override
--- trunk/src/com/kumquat/hybris/databases/UPCDatabaseHelper.java
+++ trunk/src/com/kumquat/hybris/databases/UPCDatabaseHelper.java
@@ -1,25 +1,36 @@
 package com.kumquat.hybris.databases;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.kumquat.hybris.R;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class UPCDatabaseHelper extends SQLiteOpenHelper {
+	private Context context;
 	private SQLiteDatabase upc_database;
-	public static final int VERSION = 2;
+	public static final int VERSION = 3;
 	
 	private static final String upc_table = "CREATE TABLE IF NOT EXISTS Upc_Table (" +
 											"id INTEGER PRIMARY KEY AUTOINCREMENT," +
 											"upc_code varchar(12) NOT NULL default ''," +
-											"upc_e varchar(8) NOT NULL default ''," +
-											"ean_code varchar(13) NOT NULL default ''," +
 											"item_id INTEGER NOT NULL," +
+											"user_added INTEGER default 0," +
 											"FOREIGN KEY (item_id) REFERENCES Items(id)" +
 											");";
 	
 	public UPCDatabaseHelper(Context context) {
 		super(context, "UPCDatabase", null, VERSION);
+		
+		this.context = context;
 	}
 
 	@Override
@@ -27,7 +38,57 @@ public class UPCDatabaseHelper extends SQLiteOpenHelper {
 		upc_database = db;
 		upc_database.execSQL(upc_table);
 		
-		// populate();
+		populate();
+	}
+	
+	private void populate() {
+		/*new Thread(new Runnable() {
+            public void run() {
+                try {
+                    loadItems();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();*/
+		
+		try {
+            loadItems();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+	}
+	
+	private void loadItems() throws IOException {
+		Log.d("UPCDatabase", "Loading items.");
+		
+		final Resources resources = context.getResources();
+        InputStream inputStream = resources.openRawResource(R.raw.upcs);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        
+        try {
+        	String line;
+        	while((line = reader.readLine()) != null) {
+        		if(line.startsWith("#")) { continue; }
+        		
+        		String[] split = line.split("\t");
+        		if(split.length != 2) { continue; }
+        		
+        		ContentValues item = new ContentValues();
+        		item.put("upc_code", split[0].trim());
+        		item.put("item_id", split[1].trim());
+        		
+        		long id = upc_database.insertOrThrow("Upc_Table", null, item);
+        		
+        		if(id == -1) {
+        			Log.e("UPCDatabase", "Unable to add: " + line.trim());
+        		}
+        	}
+        } finally {
+        	reader.close();
+        }
+		
+		Log.d("UPCDatabase", "Done loading items.");
 	}
 
 	@Override
