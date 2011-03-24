a6ee84ab70c0f4ee3bb9ad77ffd7c2dcecc0c7da
kesshoryu927 <kesshoryu927@gmail.com>
2011-03-24 21:39:16 +0000

    Made a bunch of logic for the manual addition page. Since we don't have an inventory yet it doesn't actually do anything.

--- gen/com/kumquat/hybris/R.java
+++ gen/com/kumquat/hybris/R.java
@@ -8,35 +8,54 @@
 package com.kumquat.hybris;
 
 public final class R {
+    public static final class array {
+        public static final int MA_Quantity_array=0x7f060003;
+        public static final int MA_Type_1_array=0x7f060000;
+        public static final int MA_Type_2_array=0x7f060001;
+        public static final int MA_Type_3_array=0x7f060002;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int code=0x7f06000c;
-        public static final int error=0x7f06000f;
-        public static final int front_add=0x7f060004;
-        public static final int front_devices=0x7f060007;
-        public static final int front_recipes=0x7f060006;
-        public static final int front_remove=0x7f060005;
-        public static final int item=0x7f06000d;
-        public static final int linearLayout1=0x7f060009;
-        public static final int manual=0x7f06000a;
-        public static final int manualentry=0x7f06000b;
-        public static final int other=0x7f06000e;
-        public static final int scan=0x7f060008;
-        public static final int textView1=0x7f060000;
-        public static final int textView2=0x7f060001;
-        public static final int textView3=0x7f060002;
-        public static final int textView4=0x7f060003;
+        public static final int code=0x7f070018;
+        public static final int error=0x7f07001b;
+        public static final int front_add=0x7f070004;
+        public static final int front_devices=0x7f070007;
+        public static final int front_recipes=0x7f070006;
+        public static final int front_remove=0x7f070005;
+        public static final int item=0x7f070019;
+        public static final int linearLayout1=0x7f070015;
+        public static final int manual=0x7f070016;
+        public static final int manualadd_cancel=0x7f070012;
+        public static final int manualadd_ok=0x7f070013;
+        public static final int manualadd_qtype=0x7f070010;
+        public static final int manualadd_quantity=0x7f070011;
+        public static final int manualadd_quantitytext=0x7f07000f;
+        public static final int manualadd_text=0x7f070008;
+        public static final int manualadd_type1=0x7f07000a;
+        public static final int manualadd_type1text=0x7f070009;
+        public static final int manualadd_type2=0x7f07000c;
+        public static final int manualadd_type2text=0x7f07000b;
+        public static final int manualadd_type3=0x7f07000e;
+        public static final int manualadd_type3text=0x7f07000d;
+        public static final int manualentry=0x7f070017;
+        public static final int other=0x7f07001a;
+        public static final int scan=0x7f070014;
+        public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070001;
+        public static final int textView3=0x7f070002;
+        public static final int textView4=0x7f070003;
     }
     public static final class layout {
         public static final int credits=0x7f030000;
         public static final int front=0x7f030001;
         public static final int list=0x7f030002;
-        public static final int splashscreen=0x7f030003;
-        public static final int test=0x7f030004;
+        public static final int manualadd=0x7f030003;
+        public static final int splashscreen=0x7f030004;
+        public static final int test=0x7f030005;
     }
     public static final class raw {
         public static final int items=0x7f040000;
@@ -44,6 +63,10 @@ public final class R {
         public static final int upcs=0x7f040002;
     }
     public static final class string {
+        public static final int MA_Quantity=0x7f050006;
+        public static final int MA_Type_1=0x7f050003;
+        public static final int MA_Type_2=0x7f050004;
+        public static final int MA_Type_3=0x7f050005;
         public static final int Test=0x7f050002;
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
--- src/com/kumquat/hybris/Item.java
+++ src/com/kumquat/hybris/Item.java
@@ -31,9 +31,24 @@ public class Item {
 		return getFromDatabase(idb, id);
 	}
 	
+	public static int findIDFromDatabase(SQLiteDatabase db, String type, String sub, String spec) {
+		String sql = "SELECT id FROM Items WHERE type = '" + type + "' AND sub_type = '" + sub + 
+						"' AND specific_type = '" + spec + "'";
+		Cursor c = db.rawQuery(sql, null);
+		
+		if(c == null || c.getCount() == 0) {
+			if(c != null) { c.close(); }
+			
+			return -1;
+		}
+		
+		c.moveToFirst();
+		return c.getInt(0);
+	}
+	
 	public static Item getFromDatabase(SQLiteDatabase db, int id) {
 		String sql_statement = "SELECT type, sub_type, specific_type FROM Items WHERE id = " + id;
-		Cursor c = db.rawQuery(sql_statement, new String[]{});
+		Cursor c = db.rawQuery(sql_statement, null);
 		
 		if(c == null || c.getCount() == 0) {
 			if(c != null) { c.close(); }
@@ -52,7 +67,7 @@ public class Item {
 	
 	public static String[] getAllTypes(SQLiteDatabase db) {
 		if(db == null) { return null; }
-		String sql = "SELECT DISTINCT type FROM Items WHERE 1 = 1 ORDER BY type";
+		String sql = "SELECT DISTINCT type FROM Items ORDER BY type";
 		Cursor c = db.rawQuery(sql, null);
 		
 		if(c == null || c.getCount() == 0) {
@@ -75,7 +90,7 @@ public class Item {
 	}
 	
 	public static String[] getAllSubTypes(SQLiteDatabase db, String type) {
-		String sql = "SELECT DISTINCT sub_type FROM Items WHERE type = " + type + " ORDER BY type";
+		String sql = "SELECT DISTINCT sub_type FROM Items WHERE type = '" + type + "' ORDER BY sub_type";
 		Cursor c = db.rawQuery(sql, null);
 		
 		if(c == null || c.getCount() == 0) {
@@ -98,7 +113,8 @@ public class Item {
 	}
 	
 	public static String[] getAllSpecificTypes(SQLiteDatabase db, String type, String subtype) {
-		String sql = "SELECT DISTINCT specific_type FROM Items WHERE type = " + type + " AND sub_type = " + subtype + " ORDER BY type";
+		String sql = "SELECT DISTINCT specific_type FROM Items WHERE type = '" + type + "' AND sub_type = '" + subtype +
+						"' ORDER BY specific_type";
 		Cursor c = db.rawQuery(sql, null);
 		
 		if(c == null || c.getCount() == 0) {
