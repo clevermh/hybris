8c0bacbd1df47fb9b0158ed35eb672002c3f3542
showdown345 <showdown345@gmail.com>
2011-03-24 20:55:39 +0000

    Created the Recipe constructor.

--- src/com/kumquat/hybris/Recipe.java
+++ src/com/kumquat/hybris/Recipe.java
@@ -1,8 +1,20 @@
 package com.kumquat.hybris;
 
 public class Recipe {
-	private int[] ingredients;
-	private int[] qtys;
-	private String name;
+	private Ingredients[] ingredients;
 	private String[] directions;
+	String prep_time;
+	String cook_time;
+	String serving_size;
+	String type;
+	
+	public Recipe(Ingredients[] ingredients, String[] directions, String prep_time, String cook_time, String serving_size, String type) {
+		this.ingredients = ingredients;
+		this.directions = directions;
+		this.prep_time = prep_time;
+		this.cook_time = cook_time;
+		this.serving_size = serving_size;
+		this.type = type;
+	}
+	
 }
