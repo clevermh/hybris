1edf3e4719f903e7c856214092efffa2381bcfbb
kesshoryu927 <kesshoryu927@gmail.com>
2011-03-31 21:14:02 +0000

    Made it show the recipe activity on a button that will disappear eventually.

--- src/com/kumquat/hybris/RecipeActivity.java
+++ src/com/kumquat/hybris/RecipeActivity.java
@@ -1,7 +1,11 @@
 package com.kumquat.hybris;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class RecipeActivity extends Activity {
-
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recipedisplay);
+	}
 }
