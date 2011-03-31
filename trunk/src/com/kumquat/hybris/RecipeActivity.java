b0f661151230291cebedac6ae75f36fc86ed1b29
SirMattDallas@gmail.com <SirMattDallas@gmail.com@f1c88aba-0dde-0a15-9544-746dbaf2be2f>
2011-03-31 22:12:32 +0000

    Added actions for zooming in and out on recipe instructions

--- trunk/src/com/kumquat/hybris/RecipeActivity.java
+++ trunk/src/com/kumquat/hybris/RecipeActivity.java
@@ -1,11 +1,36 @@
 package com.kumquat.hybris;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.widget.ZoomControls;
 
 public class RecipeActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.recipedisplay);
+		
+		final ZoomControls zoomer = (ZoomControls)findViewById(R.id.zoom_on_text);
+		final TextView txt_instruction = (TextView)findViewById(R.id.text_instruct);
+		zoomer.setOnZoomInClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// Go to the remove page
+				txt_instruction.setTextSize(txt_instruction.getTextSize());
+			}
+		});
+		zoomer.setOnZoomOutClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// Go to the remove page
+				txt_instruction.setTextSize((float)txt_instruction.getTextSize() * (float)0.5);
+				txt_instruction.setText("text");
+			}
+		});
+		
+		
 	}
 }
